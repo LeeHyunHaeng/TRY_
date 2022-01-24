426272d23b8a2ddf2795a3b8c59b2a906e5ffda2
Haeng <onse3840@gmail.com>
2022-01-24 17:49:51 +0900

    Binding 자바 -> 코틀린 변환

--- sample/src/main/java/com/yjk/sample/bind_kotlin/ActivityBindMain.kt
+++ sample/src/main/java/com/yjk/sample/bind_kotlin/ActivityBindMain.kt
@@ -3,28 +3,23 @@ package com.yjk.sample.bind_kotlin
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.annotation.ContentView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.yjk.common.view.base.BaseActivity
+import com.yjk.sample.R
 import com.yjk.sample.databinding.ActivitybindingmainKotlinBinding
 
-class ActivityBindMain : BaseActivity() {
+class ActivityBindMain : AppCompatActivity() {
+    private lateinit var binding : ActivitybindingmainKotlinBinding
 
-    private lateinit var mBinding : ActivitybindingmainKotlinBinding
-
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        mBinding = ActivitybindingmainKotlinBinding.inflate(layoutInflater);
-
-
-        initView()
-        setEvent()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activitybindingmain_kotlin)
+        binding.dataProfile = DataProfile("이행수","2살", 0)
     }
 
-    override fun initView() {
-        TODO("Not yet implemented")
-    }
-
-    override fun setEvent() {
-        TODO("Not yet implemented")
-    }
 }
+
+
+
+
---
+++ sample/src/main/java/com/yjk/sample/bind_kotlin/DataProfile.kt
@@ -0,0 +1,8 @@
+package com.yjk.sample.bind_kotlin
+
+class DataProfile(val name : String,val age : String, val gender : Int){
+
+    val GENDER_MALE : Int = 0
+    val GENDER_FEMALE : Int = 1
+
+}
