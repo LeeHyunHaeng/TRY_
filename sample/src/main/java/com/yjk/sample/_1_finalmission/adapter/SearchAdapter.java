1bc0dee4e30d7a977433891a94fc5b0eb0f87791
Haeng <onse3840@gmail.com>
2022-01-28 09:23:08 +0900

    MiniYoutube 하는 중 (1/28)

--- sample/src/main/java/com/yjk/sample/_1_finalmission/adapter/SearchAdapter.java
+++ sample/src/main/java/com/yjk/sample/_1_finalmission/adapter/SearchAdapter.java
@@ -25,7 +25,6 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.mViewHolde
 
     ArrayList<SearchData> mList;
     Context context;
-    Activity1RecyclerviewItemBinding binding;
 
     public SearchAdapter(Context context, ArrayList<SearchData> list) {
         this.context = context;
@@ -36,26 +35,21 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.mViewHolde
     @Override
     public mViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        binding = Activity1RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        Activity1RecyclerviewItemBinding binding = Activity1RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
         return new mViewHolder(binding);
     }
-        //item_utube xml파일을 객체화 시킨다.
-//        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        View view = inflater.inflate(R.layout.activity_1_recyclerview_item, parent, false);
-//        return new mViewHolder(view);
-//    }
 
     @Override
     public void onBindViewHolder(@NonNull mViewHolder holder, int position) {
 
         //영상제목 셋팅
-        holder.title.setText(mList.get(position).getTitle());
+        holder.binding.title.setText(mList.get(position).getTitle());
 
         //이미지를 넣어주기 위해 이미지url을 가져와서 썸에일 적용.
         String imageUrl = mList.get(position).getImageUrl();
-        Glide.with(holder.imageView)
+        Glide.with(holder.binding.titleImage)
                 .load(imageUrl)
-                .into(holder.imageView);
+                .into(holder.binding.titleImage);
 
 //        //채널명 셋팅
 //        binding.channel.setText(mList.get(position).getChannelId());
@@ -71,16 +65,11 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.mViewHolde
     }
 
     public class mViewHolder extends RecyclerView.ViewHolder{
-        private TextView title,channel,views;
-        private ImageView imageView;
+        private Activity1RecyclerviewItemBinding binding;
 
-        public mViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            title = itemView.findViewById(R.id.title);
-            channel = itemView.findViewById(R.id.channel);
-            views = itemView.findViewById(R.id.views);
-            imageView = itemView.findViewById(R.id.titleImage);
+        public mViewHolder(@NonNull Activity1RecyclerviewItemBinding b) {
+            super(b.getRoot());
+            binding = b;
 
 //            //유튜브 영상을 클릭하면 재생이 되는 액티비티로 이동
 //            itemView.setOnClickListener(new View.OnClickListener() {
